cf86dc02d118f527da6ddd91944fcf549363ce0c
Michael J. Sullivan <sully@msully.net>
2018-04-27 16:31:05 -0700

    tweaks

--- scripts/build-debug-python.sh
+++ scripts/build-debug-python.sh
@@ -2,7 +2,7 @@
 
 VERSION=3.6.5
 MAJOR_VERSION=$(echo $VERSION | cut -d. -f1-2)
-PREFIX=~/python-$MAJOR_VERSION-debug/
+PREFIX=~/python-$MAJOR_VERSION-debug
 if [[ -f $PREFIX/bin/python3 ]]; then
     exit
 fi
@@ -15,3 +15,4 @@ make -j4
 make install
 $PREFIX/bin/pip3 install virtualenv
 $PREFIX/bin/virtualenv ~/virtualenv/python-$MAJOR_VERSION-debug/
+ln -s python3-config $PREFIX/bin/python-config
