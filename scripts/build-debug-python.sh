1ce4f6342216ea56ca653dd394742c1e686fbd77
Michael J. Sullivan <sully@msully.net>
2018-04-27 15:15:12 -0700

    Try a different tactic

---
+++ scripts/build-debug-python.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eu
+VERSION=3.6.5
+MAJOR_VERSION=$(echo $VERSION | cut -d. -f1-2)
+PREFIX=~/python-$MAJOR_VERSION-debug/
+
+wget https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
+tar zxf Python-$VERSION.tgz
+pushd Python-$VERSION
+./configure CFLAGS='-DPy_DEBUG -DPy_TRACE_REFS -DPYMALLOC_DEBUG' --with-pydebug --prefix=$PREFIX
+make -j4
+make install
+$PREFIX/pip3 install virtualenv
+$PREFIX/virtualenv ~/virtualenv/python-$MAJOR_VERSION-debug/
